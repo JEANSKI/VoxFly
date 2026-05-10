4a08cd521ef1650c4b544d2c8c1da75b30e16906
JEANSKI <85173839+J34NSK1@users.noreply.github.com>
2026-05-10 16:11:12 -0500

    Crea clase Pajaro aplicando sobrecarga, herencia, gravedad y salto

---
+++ src/main/java/modelo/Pajaro.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.awt.Color;
+import java.awt.Graphics;
+/**
+ *
+ * @author User
+ */
+public class Pajaro extends EntidadJuego {
+    private int velocidadY;
+    private static final int GRAVEDAD = 2;
+    private static final int FUERZA_SALTO = -15;
+
+    // Constructor completo
+    public Pajaro(int x, int y, int ancho, int alto) {
+        super(x, y, ancho, alto);
+        this.velocidadY = 0;
+    }
+
+    // Polimorfismo estático
+    public Pajaro(int x, int y) {
+        this(x, y, 30, 30); // Tamaño por defecto
+    }
+
+    @Override
+    public void mover() {
+        velocidadY += GRAVEDAD;
+        y += velocidadY;
+        
+        // Limites de pantalla
+        if (y > 600) y = 600;
+        if (y < 0) { y = 0; velocidadY = 0; }
+    }
+
+    public void saltar() {
+        velocidadY = FUERZA_SALTO;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(x, y, ancho, alto);
+    }
+}
